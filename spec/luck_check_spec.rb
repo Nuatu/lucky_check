215fc910fd19f4f9219dad7638ee52828024b82d
Nuatu <nuatut@gmail.com>
2014-07-29 00:06:04 -0700

    refactor specs

--- spec/luck_check_spec.rb
+++ spec/luck_check_spec.rb
@@ -2,11 +2,11 @@ require 'rspec'
 require 'luck_check'
 
 describe 'luck_check' do
-	it "takes a number and returns TRUE if the left half IS equal to the right half" do
+	it "takes a number and returns 'true' if the left half IS equal to the right half" do
 		expect(luck_check('111012')).to eq(true)
 	end
 
-	it "takes a number and returns FALSE if the left half IS NOT equal to the right half" do
+	it "takes a number and returns 'false' if the left half IS NOT equal to the right half" do
 		expect(luck_check('111123')).to eq(false)
 	end
 end
