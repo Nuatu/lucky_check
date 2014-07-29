9001fef975d892809b557f77f90d77ace8b1982c
Nuatu <nuatut@gmail.com>
2014-07-28 23:49:06 -0700

    def luck_checker (left, right) left == right ? true : false
    exit

---
+++ lib/luck_check.rb
@@ -0,0 +1,20 @@
+def luck_check (input)
+	numbers = input.split('')
+	halfway_point = numbers.length / 2
+	#splits the input into an array
+
+	left = sum_calc(numbers[0,halfway_point])
+	right = sum_calc(numbers[halfway_point..-1])
+	#passes left/right halves of array to 'sum_calc' function below
+
+	luck_checker(left, right)
+	#passes left and right to 'luck_checker' function below
+end
+
+def sum_calc (input)
+	sum = 0
+	input.each do |x|
+		sum += x.to_i
+	end
+	sum
+end
