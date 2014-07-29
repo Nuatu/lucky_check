c46f3277d0ae27bf0b5618e0a5f7256b28253475
Nuatu <nuatut@gmail.com>
2014-07-29 01:13:52 -0700

    test to check if input string has even amount of characters

--- spec/luck_check_spec.rb
+++ spec/luck_check_spec.rb
@@ -6,7 +6,11 @@ describe 'luck_check' do
 		expect(luck_check('111012')).to eq(true)
 	end
 
-	it "takes a number and returns 'false' if the left half IS NOT equal to the right half" do
+	it "takes a number and returns 'false' if the left half is NOT equal to the right half" do
 		expect(luck_check('111123')).to eq(false)
+	end
+
+	it "takes a number and returns 'invalid' if the length of input is NOT even" do
+		expect(luck_check('111')).to eq("input string is invalid, odd number of characters")
 	end
 end
