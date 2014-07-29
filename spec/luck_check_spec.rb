09a53feb604b23603679c406013e5aa851faf004
Nuatu <nuatut@gmail.com>
2014-07-28 23:45:35 -0700

    update specs to use expect instead of should

--- spec/luck_check_spec.rb
+++ spec/luck_check_spec.rb
@@ -3,10 +3,10 @@ require 'luck_check'
 
 describe 'luck_check' do
 	it "takes a number and returns TRUE if the left half IS equal to the right half" do
-		luck_check('111012').should(eq(true))
+		expect(luck_check('111012')).to eq(true)
 	end
 
 	it "takes a number and returns FALSE if the left half IS NOT equal to the right half" do
-		luck_check('111123').should(eq(false))
+		expect(luck_check('111123')).to eq(false)
 	end
 end
