7e018703ed19a6078aa32b9c29a82659c3d452e9
Nuatu <nuatut@gmail.com>
2014-07-28 22:21:49 -0700

    add test spec coverage for expected outcomes

--- spec/luck_check_spec.rb
+++ spec/luck_check_spec.rb
@@ -1,1 +1,12 @@
+require 'rspec'
+require 'luck_check'
 
+describe 'luck_check' do
+	it "takes a number and returns TRUE if the left half IS equal to the right half" do
+		luck_check('111012').should(eq(true))
+	end
+
+	it "takes a number and returns FALSE if the left half IS NOT equal to the right half" do
+		luck_check('111123').should(eq(false))
+	end
+end
