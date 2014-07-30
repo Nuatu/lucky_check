394f240c2b5163023e076cf6095095994cdbdbdc
Nuatu <nuatut@gmail.com>
2014-07-29 18:55:29 -0700

    fixed indentation

--- lib/luck_check.rb
+++ lib/luck_check.rb
@@ -1,28 +1,28 @@
 def luck_check (input)
-	numbers = input.split('')
-	halfway_point = (numbers.length / 2)
-	#splits the input into an array
+  numbers = input.split('')
+  halfway_point = (numbers.length / 2)
+  #splits the input into an array
 
-	left = sum_calc(numbers[0,halfway_point])
-	right = sum_calc(numbers[halfway_point..-1])
-	#passes left/right halves of array to 'sum_calc' function below
+  left = sum_calc(numbers[0,halfway_point])
+  right = sum_calc(numbers[halfway_point..-1])
+  #passes left and right halves of array to 'sum_calc' function below
 
-	luck_checker(numbers, left, right)
-	#passes left and right to 'luck_checker' function below
+  luck_checker(numbers, left, right)
+  #passes left and right to 'luck_checker' function below
 end
 
 def luck_checker (numbers, left, right)
-	if numbers.length.even?
-		left == right ? true : false 
-	else
-		"input string is invalid, odd number of characters"
-	end
-	#checks to see if numbers has even elements, and returns true, false, or invalid
+  if numbers.length.even?
+	  left == right ? true : false 
+  else
+	  "input string is invalid, odd number of characters"
+  end
+  #checks to see if numbers has even elements, and returns true, false, or invalid
 end
 
 def sum_calc (input)
-	sum = 0
-	input.each do |x| sum += x.to_i end
-	sum
-	#loops through the input array and sums the elements
+  sum = 0
+  input.each do |x| sum += x.to_i end
+  sum
+  #loops through the input array and sums the elements
 end
--- spec/luck_check_spec.rb
+++ spec/luck_check_spec.rb
@@ -2,15 +2,15 @@ require 'rspec'
 require 'luck_check'
 
 describe 'luck_check' do
-	it "takes a number and returns 'true' if the left half IS equal to the right half" do
-		expect(luck_check('111012')).to eq(true)
-	end
+  it "takes a number and returns 'true' if the left half IS equal to the right half" do
+	  expect(luck_check('111012')).to eq(true)
+  end
 
-	it "takes a number and returns 'false' if the left half is NOT equal to the right half" do
-		expect(luck_check('111123')).to eq(false)
-	end
+  it "takes a number and returns 'false' if the left half is NOT equal to the right half" do
+    expect(luck_check('111123')).to eq(false)
+  end
 
-	it "takes a number and returns 'invalid' if the length of input is NOT even" do
-		expect(luck_check('111')).to eq("input string is invalid, odd number of characters")
-	end
+  it "takes a number and returns 'invalid' if the length of input is NOT even" do
+    expect(luck_check('111')).to eq("input string is invalid, odd number of characters")
+  end
 end
